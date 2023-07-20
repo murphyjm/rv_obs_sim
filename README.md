549182c5556a1c392794603bfc6d1c469c01d172
Joey Murphy <joseph.murphy@ucsc.edu>
2023-07-20 14:49:57 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simulate RV observations
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rv_obs_sim',
+      version='0.1',
+      # list folders, not files
+      packages=['rv_obs_sim'],
+
+)
