da2f9289acab7a8d7427b9a6e41f5d07b1a1de51
Joey Murphy <joseph.murphy@ucsc.edu>
2023-07-24 13:03:31 -0400

    Fixed resampling data

--- rv_obs_sim/dataSim.py
+++ rv_obs_sim/dataSim.py
@@ -11,20 +11,23 @@ class DataSim:
         
         ***For now, only trust this object when using a dataset from a single telescope***
         '''
-        assert (nobs > 0 and min_obs_cadence > 0), 'nobs and obs_cadence must both be > 0.'
+        assert (nobs >= 0 and min_obs_cadence >= 0), 'nobs and obs_cadence must both be >= 0.'
         self.nobs = nobs
         self.min_obs_cadence = min_obs_cadence
         self.obs_start, self.obs_end = obs_start_end
 
         self.data = None
     
-    def load_data(self, data_path, read_csv_kwargs) -> None:
-    
+    def load_data(self, data_path, **read_csv_kwargs) -> None:
+
         try:
             df = pd.read_csv(data_path, **read_csv_kwargs)
         except FileNotFoundError:
             print(f"Warning: Data path {data_path} does not exist.")
             return
+
+        if 'bjd' in df.columns.tolist():
+            df = df.rename(columns={'time':'date', 'bjd':'time'})
 
         assert set(['time', 'mnvel','errvel', 'tel']).issubset(df.columns), "Data must have columns: 'time', 'mnvel', 'errvel', 'tel'."
         
@@ -46,10 +49,22 @@ class DataSim:
         '''
         Resample the data according to the specified number of observations, cadence, and start/end dates.
         '''
-        mask = self.data['time'] > self.obs_start
-        mask &= self.data['time'] < self.obs_end
-        mask &= np.array([1] + [np.ediff1d(self.data['time']) >= self.min_obs_cadence], dtype=bool)
-        mask &= np.arange(len(self.data)) < self.nobs
+        good_inds = []
+        time_range_mask = self.data['time'] > self.obs_start
+        time_range_mask &= self.data['time'] < self.obs_end
+        inds = self.data[time_range_mask].index
+        good_inds.append(inds[0])
+        prev_time = self.data.loc[good_inds[0], 'time']
 
-        self.df_resampled = self.data[mask]
+        for i in range(1, len(inds)):
+            if len(good_inds) >= self.nobs:
+                break
+            ind = inds[i]
+            if (self.data.loc[ind, 'time'] - prev_time) >= self.min_obs_cadence:
+                good_inds.append(ind)
+                prev_time = self.data.loc[ind, 'time']
+            else:
+                continue
+
+        self.df_resampled = self.data.iloc[good_inds]
         return self.df_resampled
